c63603d359a4996949a53ef45eae7d1f050cc634
Michael Große <micgro42@physik.hu-berlin.de>
2014-09-27 20:09:51 +0200

    Add exception for Errors during parsing some lines
    There are occasions where the parsing of a line could fail. This
    exceptions needs the failed line as mandatory first argument and takes a
    message as optional second argument.

--- sanedecayrescaling/utility.py
+++ sanedecayrescaling/utility.py
@@ -8,3 +8,8 @@ def open_file_safely(path_to_file, modus):
         print 'cannot open', path_to_file
         raise SystemExit(os.EX_IOERR)
     return open_file
+
+class ParseError(Exception):
+    def __init__(self, line , msg = 'some parsing went wrong'):
+        self.line = line
+        self.msg = msg
