cd71d1712ccfe8cabe67cb39198defd595c9d230
Michael Große <micgro42@physik.hu-berlin.de>
2014-09-30 09:33:17 +0200

    Rename fixture file and extend it for future tests

--- sanedecayrescaling/tests/test_extract_decays.py
+++ sanedecayrescaling/tests/test_extract_decays.py
@@ -7,19 +7,24 @@ import pytest
 @fixture  # Registering this function as a fixture.
 def fixture_source(request):
     print "start setup"
-    SourceDecayFile=open('SourceDecayFile.dec.tmp','w')
-    SourceDecayFile.write("some lines at start\n")
-    SourceDecayFile.write("Decay B0\n")
-    SourceDecayFile.write("some lines 1\n")
-    SourceDecayFile.write("some lines 2\n")
-    SourceDecayFile.write("some lines 3\n")
-    SourceDecayFile.write("some lines 4\n")
-    SourceDecayFile.write("some lines 5\n")
-    SourceDecayFile.write("some lines 6\n")
-    SourceDecayFile.write("Enddecay\n")
-    SourceDecayFile.write("some lines at end\n")
-    SourceDecayFile.write("last line of the file")
-    SourceDecayFile.close()
+    source_decay_file=open('source_decay_file.dec.tmp','w')
+    source_decay_file.write("some lines at start\n")
+    source_decay_file.write("Decay B0\n")
+    source_decay_file.write("some lines 1\n")
+    source_decay_file.write("some lines 2\n")
+    source_decay_file.write("some lines 3\n")
+    source_decay_file.write("some lines 4\n")
+    source_decay_file.write("some lines 5\n")
+    source_decay_file.write("some lines 6\n")
+    source_decay_file.write("Enddecay\n")
+    source_decay_file.write("some lines at end\n")
+    source_decay_file.write("Decay D*+\n")
+    source_decay_file.write("0.6770    D0  pi+ VSS;\n")
+    source_decay_file.write("0.3070    D+  pi0 VSS;\n")
+    source_decay_file.write("0.0160    D+  gamma VSP_PWAVE;\n")
+    source_decay_file.write("Enddecay\n")
+    source_decay_file.write("last line of the file")
+    source_decay_file.close()
 
     reference_file=open('reference_file.tmp','w')
     reference_file.writelines(["=============\n",
@@ -74,14 +79,14 @@ def fixture_source(request):
     @request.addfinalizer
     def tearDown():
         print "TEAR DOWN!"
-        os.remove("SourceDecayFile.dec.tmp")
+        os.remove("source_decay_file.dec.tmp")
         if os.path.isfile("workfile.tmp"):
             os.remove("workfile.tmp")
         os.remove('reference_file.tmp')
 
 
 def test_successfully_extracting_decay(fixture_source):
-    assert blub('SourceDecayFile.dec.tmp', "B0") == 0
+    assert blub('source_decay_file.dec.tmp', "B0") == 0
     workfile=open("workfile.tmp",'r')
     for i, line in enumerate(workfile):
         if i == 0:
@@ -94,7 +99,7 @@ def test_successfully_extracting_decay(fixture_source):
 
 def test_decay_not_found(fixture_source):
     with pytest.raises(SystemExit) as cm:
-        blub('SourceDecayFile.dec.tmp', "B+")
+        blub('source_decay_file.dec.tmp', "B+")
     ex = cm.value
     assert ex.code == os.EX_DATAERR # SystemExit should be os.EX_DATAERR!
     assert not os.path.isfile("workdir.tmp") # workdir.tmp has been created even so it shouldn't have been
@@ -102,7 +107,7 @@ def test_decay_not_found(fixture_source):
 
 def test_decayfile_not_found(fixture_source):
     with pytest.raises(SystemExit) as cm:
-        blub('XSourceDecayFile.dec.tmp', "B0")
+        blub('Xsource_decay_file.dec.tmp', "B0")
     ex = cm.value
     assert ex.code == os.EX_IOERR # SystemExit should be os.EX_IOERR!
     assert not os.path.isfile("workdir.tmp") # workdir.tmp has been created even so it shouldn't have been
@@ -110,7 +115,7 @@ def test_decayfile_not_found(fixture_source):
 
 def test_particle_name_incomplete(fixture_source):
     with pytest.raises(SystemExit) as cm:
-        blub('SourceDecayFile.dec.tmp', "B")
+        blub('source_decay_file.dec.tmp', "B")
     ex = cm.value
     assert ex.code == os.EX_DATAERR # SystemExit should be os.EX_DATAERR!
     assert not os.path.isfile("workdir.tmp") # workdir.tmp has been created even so it shouldn't have been
